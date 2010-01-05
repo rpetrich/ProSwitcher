870e9e3b93f5b7d8ec85d2c3f13e2c13dfeba903
Ryan Petrich <rpetrich@gmail.com>
2010-01-05 07:39:50 -0700

    Use CHAddHook so Cycript doesn't flip out due to missing type encoding (requires CaptainHook update

--- PSWApplication.m
+++ PSWApplication.m
@@ -356,8 +356,8 @@ CHConstructor {
 	CHLoadLateClass(SBApplicationController);
 	CHLoadLateClass(SBIconModel);
 	CHLoadLateClass(SBApplication);
-	CHHook1(SBApplication, _relaunchAfterAbnormalExit);
-	CHHook0(SBApplication, _relaunchAfterExit);
+	CHAddHook1(void, SBApplication, _relaunchAfterAbnormalExit, BOOL);
+	CHAddHook0(void, SBApplication, _relaunchAfterExit);
 	CHHook1(SBApplication, defaultImage);
 	CHLoadLateClass(SBApplicationIcon);
 	CHHook1(SBApplicationIcon, setBadge);
