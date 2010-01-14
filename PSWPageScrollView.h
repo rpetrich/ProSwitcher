9ee2e5ef6dfb5540017876e8b16a9e21843c5086
Ryan Petrich <rpetrich@gmail.com>
2010-01-13 20:19:15 -0700

    Full tap edges functionality back

---
+++ PSWPageScrollView.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+@interface PSWPageScrollView : UIScrollView {
+@private
+	BOOL _shouldScrollOnUp; 
+}
+
+@end
--- PSWPageScrollView.m
+++ PSWPageScrollView.m
@@ -17,6 +17,7 @@
 		offset.x = 0.0f;
 	[self setContentOffset:offset animated:YES];
 	[self performSelector:@selector(tapPreviousAndContinue) withObject:nil afterDelay:0.5f];
+	_shouldScrollOnUp = NO;
 }
 
 - (void)tapNextAndContinue
@@ -29,42 +30,58 @@
 		if (offset.x > maxOffset)
 			offset.x = maxOffset;
 		[self setContentOffset:offset animated:YES];
-		[self performSelector:@selector(tapPreviousAndContinue) withObject:nil afterDelay:0.5f];
+		[self performSelector:@selector(tapNextAndContinue) withObject:nil afterDelay:0.5f];
 	}
+	_shouldScrollOnUp = NO;
 }
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
 	UITouch *touch = [touches anyObject];
-	NSInteger tapCount = [touch tapCount];
 	CGPoint point = [touch locationInView:[self superview]];
 	CGPoint offset = [self frame].origin;
 	point.x -= offset.x;
-	point.y -= offset.y;
 	if (point.x <= 0.0f) {
-		if (tapCount == 2) {
+		[self performSelector:@selector(tapPreviousAndContinue) withObject:nil afterDelay:0.1f];
+	} else if (point.x > [self bounds].size.width) {
+		[self performSelector:@selector(tapNextAndContinue) withObject:nil afterDelay:0.1f];
+	}
+	_shouldScrollOnUp = YES;
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+	[NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(tapPreviousAndContinue) object:nil];
+	[NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(tapNextAndContinue) object:nil];
+	_shouldScrollOnUp = NO;
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+	UITouch *touch = [touches anyObject];
+	NSInteger tapCount = [touch tapCount];
+	CGPoint point = [touch locationInView:[self superview]];
+	point.x -= [self frame].origin.x;
+	if (tapCount == 2) {
+		if (point.x <= 0.0f) {
 			[self setContentOffset:CGPointZero animated:YES];
 		} else {
-			[self tapPreviousAndContinue];
-		}
-	} else {
-		CGSize size = [self bounds].size;
-		if (point.x > size.width) {
-			if (tapCount == 2) {
+			CGSize size = [self bounds].size;
+			if (point.x > size.width) {
 				CGPoint offset;
 				CGSize contentSize = [self contentSize];
 				offset.x = (size.width < contentSize.width) ? contentSize.width - size.width : 0.0f;
 				offset.y = (size.height < contentSize.height) ? contentSize.height - size.height : 0.0f;
 				[self setContentOffset:offset animated:YES];
-			} else {
-				[self tapNextAndContinue];
 			}
 		}
+	} else if(_shouldScrollOnUp) {
+		if (point.x <= 0.0f) {
+			[self tapPreviousAndContinue];
+		} else if (point.x > [self bounds].size.width) {
+			[self tapNextAndContinue];
+		}
 	}
-}
-
-- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
-{
 	[NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(tapPreviousAndContinue) object:nil];
 	[NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(tapNextAndContinue) object:nil];
 }
@@ -73,6 +90,7 @@
 {
 	[NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(tapPreviousAndContinue) object:nil];
 	[NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(tapNextAndContinue) object:nil];
+	_shouldScrollOnUp = NO;
 }
 
 @end
