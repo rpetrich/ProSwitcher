731a1fd52dd9b0747b130e99f97c25f945bc706e
Ryan Petrich <rpetrich@gmail.com>
2010-04-11 19:52:40 -0600

    Support compiling against 3.2

--- Headers/SpringBoard+OS32.h
+++ Headers/SpringBoard+OS32.h
@@ -1,5 +1,12 @@
 #import <SpringBoard/SpringBoard.h>
 
+#if __IPHONE_OS_VERSION_MIN_REQUIRED >= __IPHONE_3_2
+@interface SBIcon (OS30)
+- (UIImage *)icon;
+- (UIImage *)smallIcon;
+@end
+#else
 @interface SBIcon (OS32)
 - (UIImage *)getIconImage:(int)sizeIndex;
 @end
+#endif
