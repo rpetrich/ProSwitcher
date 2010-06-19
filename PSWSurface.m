48e2efb97379115590ed1ebaf3e5c6e13eccdf12
Ryan Petrich <rpetrich@gmail.com>
2010-06-19 15:19:20 -0600

    Detect if surface accelerator is unavailable and ignore attempts to copy surfaces to main memory if so

--- PSWSurface.m
+++ PSWSurface.m
@@ -1,12 +1,29 @@
 #import "PSWSurface.h"
 
 #ifdef USE_IOSURFACE
+#include <sys/types.h>
+#include <sys/sysctl.h>
+
 static IOSurfaceAcceleratorRef accelerator;
+
+BOOL PSWSurfaceAcceleratorIsAvailable()
+{
+	size_t size;
+	sysctlbyname("hw.machine", NULL, &size, NULL, 0);
+	char machine[size];
+	return (strcmp(machine, "iPhone1,1") != 0)
+	    && (strcmp(machine, "iPhone1,2") != 0)
+	    && (strcmp(machine, "iPod1,1") != 0)
+	    && (strcmp(machine, "iPod2,1") != 0);
+}
 
 IOSurfaceRef PSWSurfaceCopyToMainMemory(IOSurfaceRef surface, OSType pixelFormat, NSUInteger bytesPerElement)
 {
 	if (!surface)
 		return NULL;
+	// If surface accelerator is not available, just retain the existing surface
+	if (!PSWSurfaceAcceleratorIsAvailable())
+		return (IOSurfaceRef)CFRetain(surface);
 	// Describe new surface
 	NSDictionary *dict = [NSDictionary dictionaryWithObjectsAndKeys:
 		[NSNumber numberWithUnsignedInteger:IOSurfaceGetWidth(surface)], kIOSurfaceWidth,
